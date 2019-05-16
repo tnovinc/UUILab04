51d79aeee965323fd3aa1f5483c1b31f5664db8e
tnovinc <50710651+tnovinc@users.noreply.github.com>
2019-05-16 19:20:45 +0200

    Add files via upload

--- GenetskiAlgoritam/src/uui/ga/lab04/Evolucija.java
+++ GenetskiAlgoritam/src/uui/ga/lab04/Evolucija.java
@@ -1,5 +1,22 @@
 package uui.ga.lab04;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Evolucija {
+	
+	private int brojIteracija;
+	
+	public Evolucija() {
+		try {
+			Scanner in = new Scanner(new FileReader("konfiguracija.txt"));
+			while(!in.next().equals("Iteracije"));
+			this.brojIteracija = in.nextInt();
+			in.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
--- GenetskiAlgoritam/src/uui/ga/lab04/Jedinka.java
+++ GenetskiAlgoritam/src/uui/ga/lab04/Jedinka.java
@@ -1,5 +1,27 @@
 package uui.ga.lab04;
 
+import java.util.ArrayList;
+
 public class Jedinka {
+	
+	private ArrayList<Gen> nizGena = new ArrayList<>();
+
+	public ArrayList<Gen> dohvatiGene() {
+		return nizGena;
+	}
+
+	public void postaviGene(ArrayList<Gen> nizGena) {
+		this.nizGena = nizGena;
+	}
+	
+	public void generirajGene() {
+		int broj = (int)(Math.random() * 1024);
+		while(broj != 0) {
+			int bit = broj % 2;
+			Gen gen = new Gen(bit);
+			nizGena.add(0, gen);
+			broj /= 2;
+		}
+	}
 
 }
