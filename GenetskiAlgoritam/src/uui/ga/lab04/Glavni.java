9922fc9a79d50a6ca17c251aab47756c358d1cef
tnovinc <50710651+tnovinc@users.noreply.github.com>
2019-05-17 16:26:25 +0200

    Add files via upload

--- GenetskiAlgoritam/src/uui/ga/lab04/Glavni.java
+++ GenetskiAlgoritam/src/uui/ga/lab04/Glavni.java
@@ -1,14 +1,25 @@
 package uui.ga.lab04;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Glavni {
-	private static Evolucija evolucija = new Evolucija();
+	private static PrintWriter out;
 
 	public static void main(String args[]) {
+		try {
+			out = new PrintWriter(new FileWriter("./rezultat/rezultat.txt", true));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Evolucija evolucija = new Evolucija(out);
 		evolucija.evoluiraj();
 		Jedinka najbolja = evolucija.dohvatiNajboljuJedinku();
-		System.out.println("Kraj\nNajbolja jedinka:");
-		najbolja.ispisiGene();
-		return;
+//		System.out.println("Kraj\nNajbolja jedinka:");
+		out.println("Kraj\nNajbolja jedinka:");
+		najbolja.ispisiGene(out);
+		out.close();
 	}
 	
 }
